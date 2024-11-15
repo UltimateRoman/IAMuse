dd68d5c2cbef87de8693e777c2ee98238a3a6d52
alanphil2k01 <alanphilipose@gmail.com>
2024-11-15 20:49:28 +0530

    Add frames boilerplate

---
+++ frames/app/frames/route.tsx
@@ -0,0 +1,18 @@
+import { Button } from "frames.js/next";
+import { frames } from "@/app/frames/frames";
+
+const handleRequest = frames(async (ctx) => {
+  let iAm: string | undefined;
+ 
+  if (ctx.message) {
+    iAm = (await ctx.message.walletAddress()) ?? "anonymous";
+  }
+ 
+  return {
+    image: <span>{iAm ? `I am ${iAm}` : `Click the button`}</span>,
+    buttons: [<Button action="post">Who am I?</Button>],
+  };
+});
+ 
+export const GET = handleRequest;
+export const POST = handleRequest;
