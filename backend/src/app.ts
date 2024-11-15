acae1a75c6f37427f0069381f107a6f38619882a
Dias <dias@littlebit.sg>
2024-11-16 01:48:55 +0800

    Changed API endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,16 +7,14 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.post('/joingame', (req:any, res:any) => {
-    const { ethAddress, cid, gameId } = req.body;
+app.post('/createGame', (req:any, res:any) => {
+    const { gameId } = req.body;
 
-    if (typeof ethAddress !== 'string' || typeof cid !== 'string' || typeof gameId !== 'string') {
-        return res.status(400).send('Invalid input');
-    }
     console.log(req);
     // Process the data here
 
     res.send('Data received');
 });
+
 
 export default app;
