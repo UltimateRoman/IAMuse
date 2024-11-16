537d252c3fe408bffde3b0b4fe22b3b06f5654a1
Dias <dias@littlebit.sg>
2024-11-16 18:00:49 +0800

    Modifying subgraph for child contract

--- subgraph/IAmuse/src/game-factory.ts
+++ subgraph/IAmuse/src/game-factory.ts
@@ -4,9 +4,12 @@ import {
 } from "../generated/GameFactory/GameFactory"
 import { GameCreated, OwnershipTransferred } from "../generated/schema"
 
+import {Game} from '../generated/templates'
+import {PreparedForBidding as PreparedForBiddingEvent} from '../generated/templates/Game/Game'
+
 export function handleGameCreated(event: GameCreatedEvent): void {
   let entity = new GameCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    event.params.gameId
   )
   entity.gameId = event.params.gameId
   entity.game = event.params.game
@@ -16,15 +19,15 @@ export function handleGameCreated(event: GameCreatedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+  Game.create(event.params.gameId);
+  entity.status=0;
   entity.save()
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
+export function handlePrepareForBidding(
+  event: PreparedForBiddingEvent
 ): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let entity = new Game.load()
   entity.previousOwner = event.params.previousOwner
   entity.newOwner = event.params.newOwner
 
@@ -34,3 +37,4 @@ export function handleOwnershipTransferred(
 
   entity.save()
 }
+
